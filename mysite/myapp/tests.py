f0094268e677a6659e906922adf21e86e89693d0
alexommi <alekseichikroman@gmail.com>
2026-03-11 15:33:21 +0300

    Writing some tests for auth, using mock

--- mysite/myapp/tests.py
+++ mysite/myapp/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+from unittest.mock import patch
 
-# Create your tests here.
+from requests import session
+from rest_framework import status
+from users.models import User
+from django.urls import reverse
+from rest_framework.test import APITestCase
+
+
+class TestAuthViewsTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='test_user', email='test@gmail.com', password='testpassword')
+
+        self.jwt_url = reverse('get_jwt')
+        self.callback_url = reverse('google_callback')
+
+    def test_get_jwt_tokens_success(self):
+        self.client.force_authenticate(user=self.user)
+
+        response = self.client.get(self.jwt_url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
+        self.assertEqual(response.data['user']['email'], self.user.email)
+
+    def test_get_jwt_tokens_unauthenticated(self):
+        response = self.client.get(self.jwt_url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    @patch('requests.post')
+    @patch('requests.get')
+    def test_google_callback_success(self, mock_get, mock_post):
+        mock_post.return_value.status_code = status.HTTP_200_OK
+        mock_post.return_value.json.return_value = {'access_token': 'fake_google_token'}
+
+        mock_get.return_value.status_code = status.HTTP_200_OK
+        mock_get.return_value.json.return_value = {
+            'email': 'newuser@gmail.com',
+            'id': '0001'
+        }
+
+        response = self.client.get(self.callback_url, {'code': 'fake_code'})
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+        self.assertTrue(User.objects.filter(email='newuser@gmail.com').exists())
+
+        session = self.client.session
+        self.assertIn('jwt_access', session)
+
+
+
